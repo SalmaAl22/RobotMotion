42a2650f5fe1ba1545694e5dcf811609a67b6fd4
Suyash <suyash.sharma@mail.concordia.ca>
2026-02-07 19:26:27 -0500

    Add tests for Task turn Right, turn left , PenDown status ,PenUp status

--- src/main/java/coen6761/Robot.java
+++ src/main/java/coen6761/Robot.java
@@ -35,4 +35,45 @@ public class Robot {
                 + " - Facing: " + direction.toString().toLowerCase();
     }
 
+    public void turnRight() {
+        switch (direction) {
+            case NORTH:
+                direction = RobotDirection.EAST;
+                break;
+            case EAST:
+                direction = RobotDirection.SOUTH;
+                break;
+            case SOUTH:
+                direction = RobotDirection.WEST;
+                break;
+            case WEST:
+                direction = RobotDirection.NORTH;
+                break;
+        }
+    }
+
+    public void turnLeft() {
+        switch (direction) {
+            case NORTH:
+                direction = RobotDirection.WEST;
+                break;
+            case WEST:
+                direction = RobotDirection.SOUTH;
+                break;
+            case SOUTH:
+                direction = RobotDirection.EAST;
+                break;
+            case EAST:
+                direction = RobotDirection.NORTH;
+                break;
+        }
+    }
+
+    public void penDown() {
+        penDown = true;
+    }
+
+    public void penUp() {
+        penDown = false;
+    }
 }
